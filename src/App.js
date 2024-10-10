a9e23160c3c2afdcd83861a72104b48bf326e772
AjayPalFuture <apal30100@gmail.com>
2024-10-10 10:52:13 +0530

    update project files and improve structure

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Navbar from "./component//Navbar";
+import Hero from "./component/Hero";
+import Products from "./component/Product";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import TopProducts from "./component/TopProduct";
+import Banner from "./component/Banner";
+import Subscribe from "./component/Subscribe";
+import Testimonials from "./component/Testinomial";
+import Footer from "./component/Footer";
+import Popup from "./component/Popup";
+
+const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup} />
+      <Products />
+      <TopProducts handleOrderPopup={handleOrderPopup} />
+      <Banner />
+      <Subscribe />
+      <Products />
+      <Testimonials />
+      <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+    </div>
+  );
+};
+
+export default App;
